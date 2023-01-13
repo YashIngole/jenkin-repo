07dfc3d035c046ad871db81820231a9cb5779d70
YashIngole <yashingole1000@gmail.com>
2023-01-13 17:54:47 +0530

    fixed slave3

--- slave.groovy
+++ slave.groovy
@@ -17,13 +17,14 @@ pipeline {
         steps {
 
         sh 'sudo yum install wget -y'
+        sh 'rm -rf onix-digital.zip'
+        sh 'unzip onix-digital.zip'
         sh 'sudo wget https://www.free-css.com/assets/files/free-css-templates/download/page287/onix-digital.zip'
          }
           }
     stage("extract the css template"){
 steps {
     sh 'rm -rf templatemo_565_onix_digital/assets/css/animated.css'
-        sh 'unzip onix-digital.zip'
         sh 'sudo -s'
         sh 'rm /var/www/html/index.html'
         sh 'cp -R templatemo_565_onix_digital/index.html /var/www/html/index.html '
