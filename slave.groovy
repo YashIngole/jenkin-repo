6381f367f7a08434876e2146b02c4aa30a846838
YashIngole <yashingole1000@gmail.com>
2023-01-13 15:12:03 +0530

    updated slave 3

--- slave.groovy
+++ slave.groovy
@@ -1,30 +1,38 @@
 pipeline {
     agent {
-        node {
+        
             label: 'slave'
-        }
+        
     }
     stages {
         stage("install http") {
+            steps{
 sh 'sudo yum update'            
 sh 'sudo yum install httpd -y'
 sh 'sudo systemctl start httpd'
 sh 'sudo systemctl enable httpd'
+            }
         }
     }
     stage("configure css") {
+        steps {
 
         sh 'sudo yum install wget -y'
         sh 'sudo wget https://www.free-css.com/assets/files/free-css-templates/download/page287/onix-digital.zip'
-    }
+         }
+          }
     stage("extract the css template"){
-
+steps {
         sh 'unzip onix-digital.zip'
         sh 'sudo -s'
         rm '/root/var/www/html/index.html'
         sh 'cp -R templatemo_565_onix_digital/index.html /root/var/www/html/index.html '
+}
     }
-    stage("curl"){
+    stage("curl")
+     {
+        steps {
         sh 'curl 18.234.215.130'
+        }
     }
 }
