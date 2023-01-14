419ba18c476904ba79d421cf9e94ea6d9e77b7af
YashIngole <yashingole1000@gmail.com>
2023-01-15 00:50:24 +0530

    http pipeline

---
+++ http.groovy
@@ -0,0 +1,34 @@
+pipeline {
+   agent any 
+
+   stages {
+    stage ("install httpd"){
+steps {
+        echo "configuring httpd"
+        sh "sudo yum update"
+        sh "sudo yum install httpd -y"
+        sh "sudo systemctl enable httpd"
+        sh "sudo systemctl start httpd"
+
+    }
+    stage ("configure css") {
+        steps {
+            echo "downloading free css template"
+
+            sh "sudo yum install wget -y"
+            sh "sudo wget https://www.free-css.com/assets/files/free-css-templates/download/page287/beautiflie.zip"
+            sh "unzip -o beautiflie.zip"
+        }
+        stage ("host index.html")
+
+        steps {
+            sh "sudo cp -R html/index.html /var/www/html/"
+            sh "curl 3.80.132.166"
+        }
+
+    }
+
+   }
+
+}
+}
