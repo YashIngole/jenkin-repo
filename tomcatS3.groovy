37fdd6f1bb84c315be5c0857c6805dc652598483
YashIngole <yashingole1000@gmail.com>
2023-01-16 16:22:06 +0530

    tomcat

--- tomcatS3.groovy
+++ tomcatS3.groovy
@@ -32,6 +32,7 @@ stage("Push War file to S3 bucket")
 {
     steps {
         echo "pushing war to s3 bucket"
+        WithAWS(credentials: 'ec2-user', region: 'us-east-1')
         sh "aws s3 mv *.war s3://rajudhep/war files/"
     }
 }
